acecc1068da83a7fb91b30108ea7c60d4c3e3e52
Samuel Arellano <a.a.r.samuel99@gmail.com>
2025-02-04 18:42:59 -0600

    updates

--- lib/presentation/views/target_presupuesto/target_presupuesto_only_text.dart
+++ lib/presentation/views/target_presupuesto/target_presupuesto_only_text.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:accounting_app/domain/request_response/get_concepts_response.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -12,13 +14,34 @@ class TargetPresupuestoOnlyText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: List<Widget>.from(getConceptsResponse.conceptos.where((x) => x.presupuesto != 0).map((x) =>
-        Row(
+        Column(
           children: [
-            Text(x.nombreConcepto),
-            Text(moneyNumberFormat.format(x.presupuesto)),
-            Text(moneyNumberFormat.format(sumaConceptos[x.nombreConcepto])),
-            Spacer(),
-            Text(moneyNumberFormat.format(x.presupuesto + (sumaConceptos[x.nombreConcepto] ?? 0)))
+            Row(
+              children: [
+                Text("${x.nombreConcepto}: "),
+                Text(moneyNumberFormat.format(x.presupuesto)),
+                Text(moneyNumberFormat.format(sumaConceptos[x.nombreConcepto] ?? 0)),
+                Spacer(),
+                Text(moneyNumberFormat.format(x.presupuesto + (sumaConceptos[x.nombreConcepto] ?? 0)))
+              ],
+            ),
+            SliderTheme(
+              data: SliderThemeData(
+                trackHeight: 20,
+              ),
+              child: Slider(
+                value: min(sumaConceptos[x.nombreConcepto]?.abs() ?? 0, x.presupuesto),
+                max: max(x.presupuesto, sumaConceptos[x.nombreConcepto]?.abs() ?? 0),
+                onChanged: (v){},
+                inactiveColor: x.presupuesto + (sumaConceptos[x.nombreConcepto] ?? 0) < 0 
+                  ? const Color.fromARGB(255, 141, 26, 26) : Colors.grey,
+                activeColor: x.presupuesto + (sumaConceptos[x.nombreConcepto] ?? 0) < 0 
+                  ? Colors.red : Colors.green,
+                thumbColor: x.presupuesto + (sumaConceptos[x.nombreConcepto] ?? 0) < 0 
+                  ? Colors.red : Colors.green,
+                allowedInteraction: SliderInteraction.tapOnly,                
+              ),
+            ),
           ],
         )
       )),
